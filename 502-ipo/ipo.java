0f728bad9b8a28bbb8040fdd2d4602fe1f210aa8
preshtha0509 <152806076+preshtha0509@users.noreply.github.com>
2026-06-20 21:01:55 +0530

    Time: 114 ms (45.39%) | Memory: 133.2 MB (55.56%) - LeetSync

---
+++ 502-ipo/ipo.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int n = profits.length;
+
+        int[][] projects = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            projects[i][0] = capital[i];
+            projects[i][1] = profits[i];
+        }
+
+        Arrays.sort(projects, (a, b) -> Integer.compare(a[0], b[0]));
+
+        PriorityQueue<Integer> maxHeap =
+                new PriorityQueue<>(Collections.reverseOrder());
+
+        int idx = 0;
+
+        for (int i = 0; i < k; i++) {
+
+            while (idx < n && projects[idx][0] <= w) {
+                maxHeap.offer(projects[idx][1]);
+                idx++;
+            }
+
+            if (maxHeap.isEmpty()) {
+                break;
+            }
+
+            w += maxHeap.poll();
+        }
+
+        return w;
+    }
+}
